7ffcbdfdf14403c6a09edbbc24acc8d988a9b002
amazarashi <eexs@foxmail.com>
2017-07-10 20:42:28 +0800

    add dashboard

---
+++ cloud-dashboard/src/main/java/com/akko/CloudDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.akko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class CloudDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDashboardApplication.class, args);
+	}
+}
